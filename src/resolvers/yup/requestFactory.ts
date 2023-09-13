6753fdfc9e232c5e1e1cc4cc5d160e6814d208c3
Dimas Leopoldo Mérida Flores <dimasmerida15@gmail.com>
2023-09-13 09:50:10 -0400

    [clear] src add

---
+++ src/resolvers/yup/requestFactory.ts
@@ -0,0 +1,62 @@
+import { NextRequest } from "next/server";
+import { IYupSchemasValid } from "./types";
+import { formatParams } from "../methods/formatParams";
+import { headers } from "next/headers";
+import { IYupRequestFactoryResp } from "./types";
+import { AnyObject, InferType, ISchema } from "yup";
+
+export const requestFactory = async <
+	B extends ISchema<any>,
+	C extends ISchema<any>,
+	Q extends ISchema<AnyObject>,
+	H extends ISchema<any>,
+	R extends ISchema<any>,
+>(
+	nativeRequest: NextRequest,
+	context: InferType<C>,
+	Schemas?: IYupSchemasValid<B, C, Q, H, R>,
+) => {
+	const body = await (async () => {
+		const valid_methods = !["DELETE", "GET"].includes(nativeRequest.method);
+
+		if (!(valid_methods && Schemas?.body)) return {};
+
+		return await nativeRequest.json();
+	})();
+
+	const resp = {
+		getHeaders: headers,
+		getContext: (): InferType<C> => {
+			const params = formatParams(context.params);
+
+			return { ...context, params };
+		},
+		getQuery: (queriesArray: string[]): InferType<Q> | undefined => {
+			//
+			if (!Object.keys(nativeRequest).includes("url")) {
+				if (Schemas?.query) return Schemas?.query.validate({});
+				return undefined;
+			}
+
+			const url = new URL(nativeRequest.url);
+
+			const resQueries: any = {};
+
+			queriesArray.map((q: string) => {
+				const validItem = Number(url.searchParams.get(q));
+				if (validItem !== 0 && !validItem) {
+					resQueries[q] = url.searchParams.get(q);
+				} else {
+					resQueries[q] = validItem;
+				}
+			});
+
+			if (Object.keys(resQueries).length) return undefined;
+
+			return resQueries;
+		},
+		getBody: (): InferType<B> => body,
+	};
+
+	return { ...resp, ...nativeRequest } as IYupRequestFactoryResp<B, C, Q>;
+};
---
+++ src/resolvers/zod/requestFactory.ts
@@ -0,0 +1,63 @@
+import { NextRequest } from "next/server";
+import { ZodType, ZodTypeDef, ZodObject, TypeOf } from "zod";
+import { IZodSchemasValid } from "./types";
+import { formatParams } from "../methods/formatParams";
+import { headers } from "next/headers";
+import { IZodRequestFactoryResp } from "./types";
+
+export const requestFactory = async <
+	B extends ZodType<any, ZodTypeDef, any>,
+	C extends ZodObject<any>,
+	Q extends ZodObject<any>,
+	H extends ZodObject<any>,
+	R extends ZodType<any, ZodTypeDef, any>,
+>(
+	nativeRequest: NextRequest,
+	context: TypeOf<C>,
+	Schemas?: IZodSchemasValid<B, C, Q, H, R>,
+) => {
+	const body = await (async () => {
+		const valid_methods = !["DELETE", "GET"].includes(nativeRequest.method);
+
+		if (!(valid_methods && Schemas?.body)) return {};
+
+		return await nativeRequest.json();
+	})();
+
+	const resp = {
+		getHeaders: headers,
+		getContext: (): TypeOf<C> => {
+			const params = formatParams(context.params);
+
+			return { ...context, params };
+		},
+		getQuery: (queriesArray: string[]): TypeOf<Q> | undefined => {
+			//
+			if (!Object.keys(nativeRequest).includes("url")) {
+				if (Schemas?.query) return Schemas?.query.parse({});
+
+				return undefined;
+			}
+
+			const url = new URL(nativeRequest.url);
+
+			const resQueries: any = {};
+
+			queriesArray.map((q: string) => {
+				const validItem = Number(url.searchParams.get(q));
+				if (validItem !== 0 && !validItem) {
+					resQueries[q] = url.searchParams.get(q);
+				} else {
+					resQueries[q] = validItem;
+				}
+			});
+
+			if (Object.keys(resQueries).length) return undefined;
+
+			return resQueries;
+		},
+		getBody: (): TypeOf<B> => body,
+	};
+
+	return { ...resp, ...nativeRequest } as IZodRequestFactoryResp<B, C, Q>;
+};
