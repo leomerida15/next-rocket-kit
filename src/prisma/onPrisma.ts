784f43b592fc600fc2dd0eaf998e5b2d10ff8847
dimasmerida15 <dimasmerida15@gmail.com>
2024-02-04 21:22:33 -0400

    [debug] correction conditionals in onPrisma

--- src/prisma/onPrisma.ts
+++ src/prisma/onPrisma.ts
@@ -5,13 +5,12 @@ export const onPrisma = <PrismaClient>(
 	PrismaClientNative: new () => PrismaClient,
 	options?: onPrismaParamsOptios,
 ) => {
-	if ((global as Global<PrismaClient>).prisma)
-		return (global as Global<PrismaClient>).prisma;
-
-	if (options?.whereGlobal)
-		return prismaAddGlobalWhere(new PrismaClientNative(), options?.whereGlobal);
-
-	if (process.env.NODE_ENV !== "production" && !(global as any).prisma) {
+	if (!(global as Global<PrismaClient>).prisma && options?.whereGlobal) {
+		(global as any).prisma = prismaAddGlobalWhere(
+			new PrismaClientNative(),
+			options?.whereGlobal,
+		);
+	} else if (process.env.NODE_ENV !== "production" && !(global as any).prisma) {
 		(global as any).prisma = new PrismaClientNative();
 	}
 
