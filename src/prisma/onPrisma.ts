29bac4412cfa944e8a139d444ed6d640a0ed28df
dimasmerida15 <dimasmerida15@gmail.com>
2024-02-04 21:30:44 -0400

    [version] 1.1.7

--- src/prisma/onPrisma.ts
+++ src/prisma/onPrisma.ts
@@ -5,17 +5,22 @@ export const onPrisma = <PrismaClient>(
 	PrismaClientNative: new () => PrismaClient,
 	options?: onPrismaParamsOptios,
 ) => {
-	if (!(global as Global<PrismaClient>).prisma && options?.whereGlobal) {
-		(global as any).prisma = prismaAddGlobalWhere(
+	if ((global as Global<PrismaClient>).prisma) {
+		return { prisma: (global as Global<PrismaClient>).prisma };
+	}
+
+	if (options?.whereGlobal) {
+		const prisma = prismaAddGlobalWhere(
 			new PrismaClientNative(),
 			options?.whereGlobal,
 		);
-	} else if (process.env.NODE_ENV !== "production" && !(global as any).prisma) {
-		(global as any).prisma = new PrismaClientNative();
+
+		(global as Global<PrismaClient>).prisma = prisma;
+
+		return { prisma: (global as Global<PrismaClient>).prisma };
 	}
 
-	const prisma =
-		(global as Global<PrismaClient>).prisma || new PrismaClientNative();
+	(global as Global<PrismaClient>).prisma = new PrismaClientNative();
 
-	return { prisma };
+	return { prisma: (global as Global<PrismaClient>).prisma };
 };
